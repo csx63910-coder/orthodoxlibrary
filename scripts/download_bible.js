1b43363b0f0f73a97891d1c28454bf85bea49aca
csx63910-coder <csx63910@gmail.com>
2026-06-03 14:19:48 +0300

    Add files via upload

--- scripts/download_bible.js
+++ scripts/download_bible.js
@@ -44,9 +44,22 @@ async function main() {
       // 3. Download all chapters for each book
       for (const book of booksData.books) {
         console.log(`  Downloading chapters for book: ${book.name} (${book.id})`);
+        
+        let subDir = '';
+         const fullBibles = ['eng_kjv', 'rus_syn', 'srp_865'];
+         if (fullBibles.includes(transId)) {
+           const ot1 = ["GEN", "EXO", "LEV", "NUM", "DEU", "JOS", "JDG", "RUT", "1SA", "2SA", "1KI", "2KI", "1CH", "2CH", "EZR", "NEH", "EST"];
+           const ot2 = ["JOB", "PSA", "PRO", "ECC", "SNG", "ISA", "JER", "LAM", "EZK", "DAN", "HOS", "JOL", "AMO", "OBA", "JON", "MIC", "NAM", "HAB", "ZEP", "HAG", "ZEC", "MAL"];
+           const nt = ["MAT", "MRK", "LUK", "JHN", "ACT", "ROM", "1CO", "2CO", "GAL", "EPH", "PHP", "COL", "1TH", "2TH", "1TI", "2TI", "TIT", "PHM", "HEB", "JAS", "1PE", "2PE", "1JN", "2JN", "3JN", "JUD", "REV"];
+
+           if (ot1.includes(book.id)) subDir = 'Old Testament 1';
+           else if (ot2.includes(book.id)) subDir = 'Old Testament 2';
+           else if (nt.includes(book.id)) subDir = 'New Testament';
+         }
+
         for (let i = 1; i <= book.numberOfChapters; i++) {
           const chapterUrl = `${API_BASE}/${transId}/${book.id}/${i}.json`;
-          const chapterPath = path.join(BASE_DIR, transId, book.id, `${i}.json`);
+          const chapterPath = path.join(BASE_DIR, transId, subDir, book.id, `${i}.json`);
           
           if (fs.existsSync(chapterPath)) {
             // console.log(`    Chapter ${i} already exists, skipping.`);
